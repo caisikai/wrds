bbdddf67c57d0405cd6b2f6a7dfe1c0fa33325e6
chenchaoyu <1415533299@qq.com>
2022-02-26 06:06:36 +0000

    add check_dump_single

--- scripts/dump_single/check_dump_single.py
+++ scripts/dump_single/check_dump_single.py
@@ -6,14 +6,14 @@ from concurrent.futures import ProcessPoolExecutor
 
 import qlib
 from qlib.data import D
-
+import os
 import fire
 import datacompy
 import pandas as pd
 from tqdm import tqdm
 from loguru import logger
-
-
+import time
+import numpy as np
 class CheckBin:
 
     NOT_IN_FEATURES = "not in features"
@@ -24,13 +24,14 @@ class CheckBin:
     def __init__(
         self,
         qlib_dir: str,
-        csv_path: str,
+        parquet_path: str,
         check_fields: str = None,
         freq: str = "day",
         symbol_field_name: str = "symbol",
         date_field_name: str = "date",
-        file_suffix: str = ".csv",
         max_workers: int = 16,
+        check_symbol_num: int = 100,
+        check_feature_num: int = 30,
     ):
         """
 
@@ -38,7 +39,7 @@ class CheckBin:
         ----------
         qlib_dir : str
             qlib dir
-        csv_path : str
+        parquet_path : str
             origin csv path
         check_fields : str, optional
             check fields, by default None, check qlib_dir/features/<first_dir>/*.<freq>.bin
@@ -48,54 +49,109 @@ class CheckBin:
             symbol field name, by default "symbol"
         date_field_name: str, optional
             date field name, by default "date"
-        file_suffix: str, optional
-            csv file suffix, by default ".csv"
         max_workers: int, optional
             max workers, by default 16
         """
         self.qlib_dir = Path(qlib_dir).expanduser()
+        self.check_symbol_num = check_symbol_num
+        self.date_field_name = date_field_name
+        self.max_workers = max_workers
+        self.freq = freq
+        
         bin_path_list = list(self.qlib_dir.joinpath("features").iterdir())
-        self.qlib_symbols = sorted(map(lambda x: x.name.lower(), bin_path_list))
+        
+        if isinstance(symbol_field_name, str):
+            symbol_field_name = symbol_field_name.split(",")
+        self.symbol_field_tuple = tuple(filter(lambda x: len(x) > 0, map(str.strip, symbol_field_name)))
+        
+        try:
+            self.catagory_mappers=self._get_mapper_dict(qlib_dir)
+            self.catagory_fields=list(self.catagory_mappers.keys())
+        except:
+            self.catagory_mappers=None
+            self.catagory_fields=None
+        if check_fields is None:
+            check_fields = list(map(lambda x: x.name.split(".")[0], bin_path_list[0].glob(f"*.bin")))
+        else:
+            check_fields = check_fields.split(",") if isinstance(check_fields, str) else check_fields
+        self.check_fields = list(map(lambda x: x.strip(), check_fields))[:check_feature_num]
+        self.check_fields = list(set(self.check_fields)|set(self.catagory_fields))
+        self.qlib_fields = list(map(lambda x: f"${x}", self.check_fields))
+        self.parquet_path = parquet_path
+        self.origin_df = pd.read_parquet(parquet_path)
+        self.symbol_field_name = self._get_symbol_field_name()
+        
+        self.origin_df[date_field_name] = pd.to_datetime(self.origin_df[date_field_name])
+        self.origin_df[[self.symbol_field_name]] = self.origin_df[[self.symbol_field_name]].astype(str)
+        self.check_symbols = self._get_check_symbols()
+        
         qlib.init(
             provider_uri=str(self.qlib_dir.resolve()),
             mount_path=str(self.qlib_dir.resolve()),
             auto_mount=False,
             redis_port=-1,
         )
-        csv_path = Path(csv_path).expanduser()
-        self.csv_files = sorted(csv_path.glob(f"*{file_suffix}") if csv_path.is_dir() else [csv_path])
+    
+        self.qlib_df = D.features(self.check_symbols, self.qlib_fields, freq=self.freq)
+        self.qlib_df.rename(columns={_c: _c.strip("$") for _c in self.qlib_df.columns}, inplace=True)
+        self.qlib_df = self._map_catagory_fields(self.qlib_df)
+        
+        self.origin_df = self.origin_df[self.origin_df[self.symbol_field_name].isin(self.check_symbols)]
+        self.origin_df.set_index([self.symbol_field_name, self.date_field_name], inplace=True)
+        self.origin_df.index.names = self.qlib_df.index.names
+        self.origin_df = self.origin_df[self.qlib_df.columns]
+        
+        self.qlib_df = self.qlib_df.reindex(self.origin_df.index)
+        self.qlib_df = self.qlib_df.astype(self.origin_df.dtypes.to_dict())
+    
+    def _get_check_symbols(self):
+        def takeSecond(elem):
+            return elem[1]
+        check_symbols = self.origin_df[self.symbol_field_name].value_counts().reset_index()
+        check_symbols = check_symbols.values.tolist()
+        check_symbols.sort(key=takeSecond)
+        check_symbols = sorted(np.array(check_symbols)[-self.check_symbol_num:,0].tolist())
+        return check_symbols
+    def _get_mapper_dict(self, uri: str):
 
-        if check_fields is None:
-            check_fields = list(map(lambda x: x.name.split(".")[0], bin_path_list[0].glob(f"*.bin")))
+        catagory_dict={}
+        dir=uri+'/catagories'
+        for file in os.listdir(dir):
+            field= file.split('.')[0]
+            with open(dir+'/'+file, 'r') as f:
+                content=f.read().splitlines()
+            catagory_dict[field]=dict(zip(range(len(content)), content))
+        return catagory_dict
+    def _map_catagory_fields(self, df:pd.DataFrame):
+        if self.catagory_mappers is None:
+            return df
+        for catafory_field_i in self.catagory_mappers: 
+            if catafory_field_i in df.columns:
+                df[catafory_field_i]=df[catafory_field_i].apply(lambda x:self.catagory_mappers[catafory_field_i].get(x))
+        return df
+    
+    def _get_symbol_field_name(self):
+        if len(self.symbol_field_tuple)==0:
+            raise ValueError("symbol field name must be specified! ")
+        elif len(self.symbol_field_tuple)==1:
+            return self.symbol_field_tuple[0]
         else:
-            check_fields = check_fields.split(",") if isinstance(check_fields, str) else check_fields
-        self.check_fields = list(map(lambda x: x.strip(), check_fields))
-        self.qlib_fields = list(map(lambda x: f"${x}", self.check_fields))
-        self.max_workers = max_workers
-        self.symbol_field_name = symbol_field_name
-        self.date_field_name = date_field_name
-        self.freq = freq
-        self.file_suffix = file_suffix
-
-    def _compare(self, file_path: Path):
-        symbol = file_path.name.strip(self.file_suffix)
-        if symbol.lower() not in self.qlib_symbols:
-            return self.NOT_IN_FEATURES
-        # qlib data
-        qlib_df = D.features([symbol], self.qlib_fields, freq=self.freq)
-        qlib_df.rename(columns={_c: _c.strip("$") for _c in qlib_df.columns}, inplace=True)
-        # csv data
-        origin_df = pd.read_csv(file_path)
-        origin_df[self.date_field_name] = pd.to_datetime(origin_df[self.date_field_name])
-        if self.symbol_field_name not in origin_df.columns:
-            origin_df[self.symbol_field_name] = symbol
-        origin_df.set_index([self.symbol_field_name, self.date_field_name], inplace=True)
-        origin_df.index.names = qlib_df.index.names
-        origin_df = origin_df.reindex(qlib_df.index)
+            #merge multi cols into one col
+            symbol_field_name="_".join(self.symbol_field_tuple)
+            self.origin_df[symbol_field_name]=""
+            for field in self.symbol_field_tuple:
+                self.origin_df[symbol_field_name]+= "_"+self.origin_df[field]
+            self.origin_df[symbol_field_name]=self.origin_df[symbol_field_name].apply(lambda x:x[1:])
+            return symbol_field_name
+    
+    def check_single(self):
+        """Check whether the bin file after ``dump_bin.py`` is executed is consistent with the original parquet file data"""
+        logger.info("start check......")
+        
         try:
             compare = datacompy.Compare(
-                origin_df,
-                qlib_df,
+                self.origin_df,
+                self.qlib_df,
                 on_index=True,
                 abs_tol=1e-08,  # Optional, defaults to 0
                 rel_tol=1e-05,  # Optional, defaults to 0
@@ -103,41 +159,12 @@ class CheckBin:
                 df2_name="New",  # Optional, defaults to 'df2'
             )
             _r = compare.matches(ignore_extra_columns=True)
-            return self.COMPARE_TRUE if _r else self.COMPARE_FALSE
+            if _r:
+                logger.info(f"compare True!")
+            else:
+                logger.info("compare False!")
         except Exception as e:
-            logger.warning(f"{symbol} compare error: {e}")
-            return self.COMPARE_ERROR
-
-    def check(self):
-        """Check whether the bin file after ``dump_bin.py`` is executed is consistent with the original csv file data"""
-        logger.info("start check......")
-
-        error_list = []
-        not_in_features = []
-        compare_false = []
-        with tqdm(total=len(self.csv_files)) as p_bar:
-            with ProcessPoolExecutor(max_workers=self.max_workers) as executor:
-                for file_path, _check_res in zip(self.csv_files, executor.map(self._compare, self.csv_files)):
-                    symbol = file_path.name.strip(self.file_suffix)
-                    if _check_res == self.NOT_IN_FEATURES:
-                        not_in_features.append(symbol)
-                    elif _check_res == self.COMPARE_ERROR:
-                        error_list.append(symbol)
-                    elif _check_res == self.COMPARE_FALSE:
-                        compare_false.append(symbol)
-                    p_bar.update()
-
-        logger.info("end of check......")
-        if error_list:
-            logger.warning(f"compare error: {error_list}")
-        if not_in_features:
-            logger.warning(f"not in features: {not_in_features}")
-        if compare_false:
-            logger.warning(f"compare False: {compare_false}")
-        logger.info(
-            f"total {len(self.csv_files)}, {len(error_list)} errors, {len(not_in_features)} not in features, {len(compare_false)} compare false"
-        )
-
-
+            logger.warning(f"compare error: {e}")
+        
 if __name__ == "__main__":
     fire.Fire(CheckBin)
